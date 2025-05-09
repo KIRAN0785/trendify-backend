a3d5739190909e95c5d55ab25a37389e30af70d6
kiran <kiran0785.be21@chitkara.edu.in>
2025-05-09 12:50:09 +0530

    s

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ app.get('/',(req,res)=>{
     res.send("API Working")
 })
 
-app.listen(port, ()=> console.log('Server started on port : '+ port))
+app.listen(port,'0.0.0.0',()=> console.log('Server started on port : '+ port))
